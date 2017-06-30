d8c1f97e365b946c82f963b5245dd38c77d79816
meekazhu <minnaliang138@gmail.com>
2017-06-30 05:43:42 -0600

    Updated README.md with necessary stuff, documented apis on routes/*.js Updated test and TODOs
    commit cf47aa5f6c200f4bf2ea3f4561a306c1b6bbdff0

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -3,9 +3,32 @@ var User = require('../models/user');
 
 const fs = require("fs");
 
+
+parse_post = function(err, res, post) {
+
+	if (err)
+		res.status(500).send(err);
+
+	let postObj = post.toObject();
+
+	delete postObj["password"]
+	delete postObj["__v"]
+	postObj["post_id"] = postObj["_id"]
+	delete postObj["_id"]
+	return postObj;
+}
+
+var post_cb = function(req, res) {
+	return function(err, post) {
+		let parseObj = parse_post(err, res, post)
+
+		res.json(parseObj);
+	}
+}
+
 module.exports.create = function(req, res, next) {
 	var fstream;
-	
+
 	req.pipe(req.busboy);
 
 	let finished = false;
@@ -31,8 +54,8 @@ module.exports.create = function(req, res, next) {
 					},
 					function(err, user) {
 						if (err)
-						res.status(500).send(err);
-				
+							res.status(500).send(err);
+
 					});
 
 				Post.update({
@@ -41,7 +64,7 @@ module.exports.create = function(req, res, next) {
 					upsert: true
 				}, (err, post) => {
 					if (err)
-					res.status(500).send(err);
+						res.status(500).send(err);
 
 					res.json({
 						'post_id': newPost.id
@@ -78,7 +101,7 @@ module.exports.updateLikeList = function(req, res, next) {
 
 			//TODO assuming this function is called last 
 			//in onion routes
-			res.send(200);
+			res.sendStatus(200);
 
 		});
 
@@ -86,13 +109,7 @@ module.exports.updateLikeList = function(req, res, next) {
 
 
 module.exports.read = function(req, res, next) {
-	Post.findById(req.params.post_id, (err, post) => {
-
-		if (err)
-			res.status(500).send(err);
-
-		res.json(post);
-	});
+	Post.findById(req.params.post_id, post_cb(req, res));
 
 }
 
@@ -105,19 +122,58 @@ module.exports.readAll = function(req, res, next) {
 	Post.find({}).sort({
 		date: 'desc'
 	}).exec(function(err, posts) {
-		if (err)
-			res.status(500).send(err);
-		res.json(posts);
+
+		var parse_posts = []
+
+		posts.forEach((post, index) => {
+			parse_posts.push(parse_post(err, res, post))
+		});
+
+		res.json(parse_posts);
 	});
 
 }
 
-module.exports.readImage = function(req, res) {
+module.exports.readAllPostsByUser = function(req, res, next) {
+	Post.find({
+		"authorId": req.params.user_id
+	}).sort({
+		date: 'desc'
+	}).exec(function(err, posts) {
+		var parse_posts = []
+
+		posts.forEach((post, index) => {
+			parse_posts.push(parse_post(err, res, post))
+		});
+		res.json(parse_posts);
+	})
+}
+
+module.exports.readImage = function(req, res, next) {
 
 	fs.readFile(__dirname + '/../../images/' + req.params.post_id, function(err, image) {
 		if (err)
 			res.status(500).send(err);
- 		res.writeHead(200, {'Content-Type': 'image/gif' });
-     	res.end(image, 'binary');
+		res.writeHead(200, {
+			'Content-Type': 'image/gif'
+		});
+		res.end(image, 'binary');
 	});
+}
+
+module.exports.readPostsById = function(req, res, next) {
+	Post.find({
+		'_id': {
+			$in: req.body.post_id
+		}
+	}).sort({
+		date: 'desc'
+	}).exec(function(err, posts) {
+		var parse_posts = []
+
+		posts.forEach((post, index) => {
+			parse_posts.push(parse_post(err, res, post))
+		});
+		res.json(parse_posts);
+	})
 }
